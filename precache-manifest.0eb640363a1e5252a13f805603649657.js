3ae71f4a9afbf6b2acc3a1220a3b09607e6b9b55
Roy Amar <royammar030783@gmail.com>
2020-08-16 12:51:20 +0300

    Updates

---
+++ precache-manifest.0eb640363a1e5252a13f805603649657.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbb8753de2995884f21110fefebc3fb",
+    "url": "/mysoundcloud/index.html"
+  },
+  {
+    "revision": "8214e045be8df348bd9c",
+    "url": "/mysoundcloud/static/css/main.993f1777.chunk.css"
+  },
+  {
+    "revision": "1d8dfec1a5f58bfd8618",
+    "url": "/mysoundcloud/static/js/2.8076ea08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mysoundcloud/static/js/2.8076ea08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8214e045be8df348bd9c",
+    "url": "/mysoundcloud/static/js/main.b4257839.chunk.js"
+  },
+  {
+    "revision": "be5c8b38f7eb1574afae",
+    "url": "/mysoundcloud/static/js/runtime-main.35aae09f.js"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/mysoundcloud/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "a6e5446a7c5789aabc9b37eaaf72134d",
+    "url": "/mysoundcloud/static/media/Oswald-VariableFont_wght.a6e5446a.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/mysoundcloud/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "104a1a91ed03ecbac6e5ce17e130cf15",
+    "url": "/mysoundcloud/static/media/noImage.104a1a91.png"
+  }
+]);
